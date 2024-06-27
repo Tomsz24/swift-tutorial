415311dfc68d0ef2910c47f7c8232eabf31deef9
Tomasz <tomaszwojciechowski244@gmail>
2024-06-27 20:16:23 +0200

    Initial Commit

---
+++ Landmark-Model/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmark-Model
+//
+//  Created by Tomasz Wojciechowski on 27/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmark-Model/Landmark_ModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark_ModelApp.swift
+//  Landmark-Model
+//
+//  Created by Tomasz Wojciechowski on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Landmark_ModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
