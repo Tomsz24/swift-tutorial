dce23f8cda79565aa7e0d7297efa5769d0c4d7a7
Tomasz <tomaszwojciechowski244@gmail>
2024-07-02 23:10:43 +0200

    preapare all assets for list and add basic functionality

---
+++ Landmark-Model/Views/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUIView.swift
+//  ToDo
+//
+//  Created by Tomasz Wojciechowski on 21/06/2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
---
+++ Landmark-Model/Views/LandmarkRow.swift
@@ -0,0 +1,35 @@
+//
+//  LandmarkRow.swift
+//  Landmark-Model
+//
+//  Created by Tomasz Wojciechowski on 02/07/2024.
+//
+
+import SwiftUI
+
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+
+
+            Spacer()
+        }
+    }
+}
+
+
+#Preview {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
+}
+    
